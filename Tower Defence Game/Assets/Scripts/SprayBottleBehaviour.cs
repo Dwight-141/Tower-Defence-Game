7e27ddeb9b90b8f967ab3c9b289a0d6377847a79
Dwight Martins <Dwight2.Martins@live.uwe.ac.uk>
2026-02-13 16:36:07 +0000

    added shooting to first unit

--- Tower Defence Game/Assets/Scripts/SprayBottleBehaviour.cs
+++ Tower Defence Game/Assets/Scripts/SprayBottleBehaviour.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 public class ShooterBehaviour : MonoBehaviour
 {
+    [Header("Rotation")] 
     public Transform target;
     public float range;
     public Vector3 direction;
     public float speed;
+
+    [Header("Shooting")]
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
 
     private void Start()
@@ -51,6 +58,25 @@ public class ShooterBehaviour : MonoBehaviour
         float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
         Quaternion lookRotation = Quaternion.AngleAxis(-angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * speed);
+
+        if (fireCountdown <=  0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject bulletIns = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        BulletBehaviour bullet = bulletIns.GetComponent<BulletBehaviour>();
+
+        if (bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected()
